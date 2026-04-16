a29a0dec3978db10ce365c4e7885f37a7e972ddd
ivansmail <ivanismail2@gmail.com>
2026-04-16 13:54:40 +0700

    add: service menu

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,6 @@
 import '../../../export.dart';
+import '../../../widgets/cache_image.dart';
+import '../../../widgets/ripple.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -37,11 +39,125 @@ class _HomeViewState extends State<HomeView> {
           ),
           Spacing(),
           Container(
+            padding: pageWrapper(),
             width: Get.width,
             child: Image.asset('assets/banner.png'),
-          )
+          ),
+          Spacing(),
+          Padding(
+            padding: pageWrapper(),
+            child: Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                borderRadius: XTheme.borderRadius(radius: 8),
+                color: XColor.netral2(),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Our \nService', style: h3n),
+                      XRipple(
+                        child: Text(
+                          'See All',
+                          style: h3n.copyWith(color: XColor.primary()),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacing(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      MenuItem(
+                        tipe: 'menu',
+                        title: 'Cut',
+                        icon: 'cut',
+                      ),
+                      MenuItem(
+                        tipe: 'menu',
+                        title: 'Beard',
+                        icon: 'beard',
+                      ),
+                      MenuItem(
+                        tipe: 'menu',
+                        title: 'Color',
+                        icon: 'color',
+                      ),
+                    ],
+                  ),
+                  Spacing()
+                ],
+              ),
+            ),
+          ),
+          Spacing(height: 20),
         ],
       )),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String? title, icon, tipe;
+  final double? size;
+  final Function()? onTap;
+  final Color? color;
+
+  const MenuItem({
+    Key? key,
+    this.title,
+    this.icon,
+    this.tipe,
+    this.onTap,
+    this.color,
+    this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          XRipple(
+            borderRadius: BorderRadius.circular(12),
+            onTap: this.onTap ?? () {},
+            child: Container(
+              padding: EdgeInsets.all(tipe == 'menu' ? 8 : 0),
+              width: this.size ?? 48,
+              height: this.size ?? 48,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: XColor.primaryLight(),
+              ),
+              child: tipe == 'menu'
+                  ? SvgPicture.asset(
+                      'assets/$icon.svg',
+                      color: this.color ?? XColor.secondary(),
+                    )
+                  : ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: XCachedImage(
+                        fit: BoxFit.fitWidth,
+                        url: icon,
+                        errorImage: title,
+                      ),
+                    ),
+            ),
+          ),
+          Spacing(height: 8),
+          Text(this.title ?? '',
+              textAlign: TextAlign.center,
+              style: h3b.copyWith(
+                color: this.color ?? XColor.netral7(),
+              ))
+        ],
+      ),
     );
   }
 }
