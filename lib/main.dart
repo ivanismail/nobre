eae5dfd5c4346e4637e1d9aaf8b780d1dc4cfaf8
ivansmail <ivanismail2@gmail.com>
2026-04-23 11:09:19 +0700

    refactore: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
-import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
+import 'app/export.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Nobre",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: XTheme.themeData(),
+      builder: (context, child) {
+        return MediaQuery(
+          child: child!,
+          data: MediaQuery.of(context).copyWith(textScaleFactor: .9),
+        );
+      },
     ),
   );
 }
