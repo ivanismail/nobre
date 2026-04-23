44d10235d713e874212981e44a217af2f681127f
ivansmail <ivanismail2@gmail.com>
2026-04-23 14:42:46 +0700

    add: view transaction item

--- lib/app/modules/transaction/views/transaction_view.dart
+++ lib/app/modules/transaction/views/transaction_view.dart
@@ -1,22 +1,88 @@
-import 'package:flutter/material.dart';
+import '../../../export.dart';
 
-import 'package:get/get.dart';
+class TransactionView extends StatefulWidget {
+  const TransactionView({super.key});
 
-import '../controllers/transaction_controller.dart';
+  @override
+  State<TransactionView> createState() => _TransactionViewState();
+}
 
-class TransactionView extends GetView<TransactionController> {
+class _TransactionViewState extends State<TransactionView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TransactionView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'TransactionView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Spacing(height: PAGE_WRAPPER),
+            TransactionItem(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: XColor.netral1(),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 6,
+            color: XColor.netral8().withOpacity(0.05),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.calendar_today,
+                    size: 14,
+                    color: XColor.primary(),
+                  ),
+                  Spacing(width: 6),
+                  Text("23 April 2026 • 15:00"),
+                ],
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                decoration: BoxDecoration(
+                  color: XColor.success().withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  'Selesai',
+                  style: h2b.copyWith(color: XColor.success()),
+                ),
+              )
+            ],
+          ),
+          Spacing(height: 5),
+          //* Price
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Potong Rambut', style: h3n),
+              Text('Rp. 25.000,-', style: h3b),
+            ],
+          ),
+        ],
       ),
     );
   }
